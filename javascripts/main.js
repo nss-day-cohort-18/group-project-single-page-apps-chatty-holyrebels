464d8835faccffaf0f48a1a6327d95e5c290cb29
Nick Nash <nicknash@Nicks-MacBook-Pro-2.local>
2017-01-27 10:23:34 -0600

    updated HTML and new JSONiife.js and main.js

---
+++ javascripts/main.js
@@ -0,0 +1,64 @@
+////// Defines global variables
+let container = document.getElementById("wrapper");
+let input = document.getElementById('input');
+let button = document.getElementById('clear-all');
+let darkBox = document.getElementById('dark');
+let largeBox = document.getElementById('large');
+let output = document.getElementById('output');
+
+////// Adds the event listeners to variables (objects)
+input.addEventListener('keypress', createMessage);
+button.addEventListener('click', clearAll);
+darkBox.addEventListener('click', darkMode);
+largeBox.addEventListener('click', largeMode);
+
+//////////////////////// Main functions
+
+////// Function that creates the message
+function createMessage(e) {
+	console.log('createMessage wants to run');
+	if (e.keycode === 13){
+		let message = input.value;
+		chatty.makeMessage(message);
+	}
+}
+
+////// Function that clears the output box
+function clearAll(e) {
+	console.log('clearAll wants to run');
+	output.innerHTML = "";
+}
+
+////// Function that toggles the background and text colors
+function darkMode(e) {
+	console.log('darkMode wants to run');
+	if(darkBox.hasAttribute('checked')){
+		container.classList.toggle('dark');   //////// NEED TO MAKE CSS CLASS
+	}
+	else {
+		container.classList.toggle('dark')
+	}
+}
+
+////// Function that toggles the font size
+function largeMode(e) {
+	console.log('largeMode wants to run');
+	if (largeBox.hasAttribute('checked')) {
+		container.classList.toggle('large');
+	}
+	else {
+		container.classList.toggle('large');
+	}
+}
+
+////// Function that prints each chat message
+function print(newMessage) {
+	console.log('print wants to run');
+	let messageDiv = "";
+	messageDiv.innerHTML = message;
+	container.appendChild(messageDiv);
+}
+ 
+
+
+

08f1ce0205d18aea801eea1afc90930bde2a65d4
Casey Dailey <casey@feasttogether.org>
2017-01-30 14:14:02 -0600

    multi-user is a go

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,7 +5,11 @@ let button = document.getElementById('clear-all');
 let darkBox = document.getElementById('dark');
 let largeBox = document.getElementById('large');
 let output = document.getElementById('output');
-
+let Casey = document.getElementById('Casey');
+let James = document.getElementById('James');
+let Nick = document.getElementById('Nick');
+let Donald = document.getElementById('Donald');
+let user = "";
 ////// This is the dynamic event handler
 let body = document.querySelector('body');
 
@@ -17,21 +21,32 @@ darkBox.addEventListener('click', darkMode);
 largeBox.addEventListener('click', largeMode);
 body.addEventListener('click', callDelete);
 
+Casey.addEventListener('change', setUserName);
+James.addEventListener('change', setUserName);
+Nick.addEventListener('change', setUserName);
+Donald.addEventListener('change', setUserName);
+
 //////////////////////// Main functions
+ function setUserName (event) {
+ 	user = event.target.id;
+ 	console.log('user:', user);
+ 	input.focus();
+ 	input.select();
 
-
-
+ }
 
 
 ////// Function that creates the message
+
+
 
 function createMessage(event) {
 	
 	if (event.keyCode === 13){
 		let message = input.value;
 		let time = Date.now();
-		Chatty.pushMessage(message, time);
-		Chatty.printMessage(message, time);
+		Chatty.pushMessage(message, time, user);
+		Chatty.printMessage(message, time, user);
 		event.currentTarget.value = "";
 		button.removeAttribute("disabled");
 	}
